39b7f3d5b3d8cfb24d0bd63f12929604232f3ecf
rabejon <rabejon@kernet.es>
2019-01-23 22:57:56 +0100

    Pasar datos de un activity a otro

--- TwoActivities/app/src/main/java/com/raul/ipartek/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/raul/ipartek/twoactivities/SecondActivity.java
@@ -1,11 +1,13 @@
 package com.raul.ipartek.twoactivities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -13,17 +15,12 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Information send", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView tW= findViewById(R.id.textView1);
+        tW.setText(message);
+
     }
 
 }
