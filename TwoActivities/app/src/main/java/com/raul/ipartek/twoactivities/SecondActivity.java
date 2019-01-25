4f5660f71b43887ac3c36f8c8d5dc84d46c5d900
rabejon <rabejon@kernet.es>
2019-01-25 19:55:37 +0100

    reciviendo datos de activity2 en activity1

--- TwoActivities/app/src/main/java/com/raul/ipartek/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/raul/ipartek/twoactivities/SecondActivity.java
@@ -7,19 +7,34 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY ="com.raul.ipartek.twoactivities.extra.REPLY";
+    private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        mReply = findViewById(R.id.editText_second);
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView tW= findViewById(R.id.textView1);
         tW.setText(message);
+
+    }
+
+    public void returnReply(View view) {
+
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK,replyIntent);
+        finish();
 
     }
 
