4aeda294700e9d0bf2926ee24990c2813baf9ab6
hobetuz <hobetuz@ipartekaula.com>
2019-01-28 19:55:20 +0100

    Estados y fix del activity 1 , el textview si contiene datos no se vuelve invisible en un cambio o reinicio de estado(landscape)

--- TwoActivities/app/src/main/java/com/raul/ipartek/twoactivities/MainActivity.java
+++ TwoActivities/app/src/main/java/com/raul/ipartek/twoactivities/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG =  MainActivity.class.getSimpleName();
     public static final String EXTRA_MESSAGE =
             "com.raul.ipartek.twoactivities.extra.MESSAGE";
+    public static final String REPLY_VISIBLE = "reply_visible";
+    public static final String REPLY_TEXT = "reply_text";
     private EditText mMessageEditText;
     public static final int TEXT_REQUEST = 1;
     private TextView mReplyHeadTextView;
@@ -20,11 +22,27 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "*********");
+        Log.d(LOG_TAG, "onCreate - MainActivity");
+
         mMessageEditText = findViewById(R.id.editText);
         mReplyHeadTextView = findViewById(R.id.text_header_reply);
         mReplyTextView = findViewById(R.id.text_message_reply);
+
+        // Restore the state.
+        if (savedInstanceState != null) {
+            boolean isVisible = savedInstanceState.getBoolean(REPLY_VISIBLE);
+
+            if (isVisible) {
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(savedInstanceState.getString("reply_text"));
+                mReplyTextView.setVisibility(View.VISIBLE);
+
+            }
+        }
     }
 
     public void launchSecondActivity(View view) {
@@ -54,5 +72,57 @@ public class MainActivity extends AppCompatActivity {
                 mReplyTextView.setVisibility(View.VISIBLE);
             }
         }
+    }
+
+    /**
+     * Guardamos los datos
+     * @param outState
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (mReplyHeadTextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean(REPLY_VISIBLE, true);
+            //Guardamos el valor del campo en el cambio de estado
+            outState.putString(REPLY_TEXT,mReplyTextView.getText().toString());
+        }
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "*********************************************");
+        Log.d(LOG_TAG, "onStart - MainActivity");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "*********************************************");
+        Log.d(LOG_TAG, "onStop - MainActivity");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "*********************************************");
+        Log.d(LOG_TAG, "onPause - MainActivity");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "*********************************************");
+        Log.d(LOG_TAG, "onResume - MainActivity");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "*********************************************");
+        Log.d(LOG_TAG, "onDestroy - MainActivity");
     }
 }
--- TwoActivities/app/src/main/java/com/raul/ipartek/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/raul/ipartek/twoactivities/SecondActivity.java
@@ -2,10 +2,8 @@ package com.raul.ipartek.twoactivities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,10 +11,13 @@ import android.widget.TextView;
 public class SecondActivity extends AppCompatActivity {
 
     public static final String EXTRA_REPLY ="com.raul.ipartek.twoactivities.extra.REPLY";
+    private static final String LOG_TAG =  SecondActivity.class.getSimpleName();
     private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "");
+        Log.d(LOG_TAG, "onCreate - SecondActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         mReply = findViewById(R.id.editText_second);
@@ -36,6 +37,41 @@ public class SecondActivity extends AppCompatActivity {
         setResult(RESULT_OK,replyIntent);
         finish();
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "*********************************************");
+        Log.d(LOG_TAG, "onStart - SecondActivity");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "*********************************************");
+        Log.d(LOG_TAG, "onStop - SecondActivity");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "*********************************************");
+        Log.d(LOG_TAG, "onPause - SecondActivity");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "*********************************************");
+        Log.d(LOG_TAG, "onResume - SecondActivity");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "*********************************************");
+        Log.d(LOG_TAG, "onDestroy - SecondActivity");
     }
 
 }
