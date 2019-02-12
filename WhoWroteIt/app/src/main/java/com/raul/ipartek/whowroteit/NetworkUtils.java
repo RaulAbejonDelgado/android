d1cde85987352374917baf5e720e2c600ab4da6e
hobetuz <hobetuz@ipartekaula.com>
2019-02-12 20:29:57 +0100

    peticion get con asyntask

---
+++ WhoWroteIt/app/src/main/java/com/raul/ipartek/whowroteit/NetworkUtils.java
@@ -0,0 +1,79 @@
+package com.raul.ipartek.whowroteit;
+
+import android.net.Uri;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static android.net.wifi.WifiConfiguration.Status.strings;
+
+public class NetworkUtils {
+
+    private static final String LOG_TAG =
+            NetworkUtils.class.getSimpleName();
+    // Base URL for Books API.
+    private static final String BOOK_BASE_URL =
+            "https://www.googleapis.com/books/v1/volumes?";
+    // Parameter for the search string.
+    private static final String QUERY_PARAM = "q";
+    // Parameter that limits search results.
+    private static final String MAX_RESULTS = "maxResults";
+    // Parameter to filter by print type.
+    private static final String PRINT_TYPE = "printType";
+
+    static String getBookInfo(String queryString) {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String bookJSONString = null;
+
+        try {
+
+            Uri builtURI = Uri.parse(BOOK_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM, queryString)
+                    .appendQueryParameter(MAX_RESULTS, "10")
+                    .appendQueryParameter(PRINT_TYPE, "books")
+                    .build();
+
+            URL requestURL = new URL(builtURI.toString());
+            urlConnection = (HttpURLConnection) requestURL.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream ip = urlConnection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(ip));
+            StringBuilder builder = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                builder.append(line);
+                builder.append("\n");
+            }
+
+            if (builder.length() == 0) {
+                return null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            //Log.d(LOG_TAG, bookJSONString);
+            return NetworkUtils.getBookInfo(strings[0]);
+
+        }
+    }
+}
+
