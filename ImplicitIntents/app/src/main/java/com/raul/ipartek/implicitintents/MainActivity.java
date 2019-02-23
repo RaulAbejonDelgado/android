4a0bf1d924f5ef53d1b7e3f430285ca5be9fdc62
rabejon <rabejon@kernet.es>
2019-02-23 22:11:45 +0100

    Control de permisos en telefono, controlando la api del dispositivo

--- ImplicitIntents/app/src/main/java/com/raul/ipartek/implicitintents/MainActivity.java
+++ ImplicitIntents/app/src/main/java/com/raul/ipartek/implicitintents/MainActivity.java
@@ -1,19 +1,29 @@
 package com.raul.ipartek.implicitintents;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.Manifest.permission.CALL_PHONE;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText mWebsiteEditText;
     private EditText mLocationEditText;
     private EditText mShareTextEditText;
+    private EditText telText;
+    private final int PHONE_CALL_CODE = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         mWebsiteEditText = findViewById(R.id.website_edittext);
         mLocationEditText = findViewById(R.id.location_edittext);
         mShareTextEditText = findViewById(R.id.share_edittext);
+
     }
 
     public void openWebsite(View view) {
@@ -69,4 +80,79 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void openTel(View view) {
+
+        telText = findViewById(R.id.telText);
+        String telephoneNumber = telText.getText().toString();
+
+        if (telephoneNumber != null && !telephoneNumber.isEmpty()) {
+            Intent intentTel = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:" + telephoneNumber));
+            //comprobamos la version de android
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                //metodo asyncrono
+                System.out.println("Api nueva 22 o superior");
+                requestPermissions(new String[] {Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
+
+            } else {
+                System.out.println("Api antigua 22 o inferior");
+                olderVersions(telephoneNumber);
+            }
+        }else{
+            Toast.makeText(MainActivity.this,"Phone number not valid",Toast.LENGTH_LONG).show();
+        }
+    }
+
+        //
+        private void olderVersions(String telephoneNumber){
+            Intent intentTel = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:"+telephoneNumber));
+            if(checkPersimission(Manifest.permission.CALL_PHONE)){
+                startActivity(intentTel);
+            }else {
+                Toast.makeText(MainActivity.this,"You declined the access",Toast.LENGTH_LONG);
+            }
+
+
+        }
+
+    public void openCamera(View view) {
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        telText = findViewById(R.id.telText);
+        String telephoneNumber = telText.getText().toString();
+       //checkeo de permisos api 22+
+        switch (requestCode){
+            //Estamos en el caso de verificar los permisos del telefono
+            case PHONE_CALL_CODE:
+                String permission = permissions[0];
+                int result = grantResults[0];
+
+                if (permission.equals(Manifest.permission.CALL_PHONE)){
+                    //comprobar si ha sido aceptado o denagada la peticion de permiso
+                    if(result == PackageManager.PERMISSION_GRANTED){
+                        //permission conceded
+                        Intent intentCall = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+ telephoneNumber));
+                        startActivity(intentCall);
+
+                    }else{
+                        //non permission conceded
+                        Toast.makeText(MainActivity.this,"You declined access",Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+            default:
+
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
+    }
+
+    private boolean checkPersimission(String permission) {
+
+        int result = this.checkCallingOrSelfPermission(permission);
+
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
 }
