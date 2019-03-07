37d4f6b8d04b365e359c584762842c750d4d51ed
rabejon <rabejon@kernet.es>
2019-03-07 22:20:28 +0100

    activity intermedia, titulos, volver atras

--- CardViewAndReciclerView/app/src/main/java/com/example/cardviewandreciclerview/ReciclerActivity.java
+++ CardViewAndReciclerView/app/src/main/java/com/example/cardviewandreciclerview/ReciclerActivity.java
@@ -1,9 +1,11 @@
 package com.example.cardviewandreciclerview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -11,47 +13,18 @@ import java.util.List;
 
 public class ReciclerActivity extends AppCompatActivity {
 
-    private List<String> names;
-
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recicler_view);
-        names = getAllNames();
-
-        recyclerView = (RecyclerView) findViewById(R.id.reciclerView);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        recyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        // specify an adapter (see also next example)
-        mAdapter = new MyAdapter(names, R.layout.recycler_view_item, new MyAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(String name, int position) {
-                Toast.makeText(ReciclerActivity.this,name + " - "+ (position +1 ),Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        recyclerView.setAdapter(mAdapter);
-
     }
 
-    private List<String> getAllNames(){
-        return new ArrayList<String>(){{
-            add("Python");
-            add("Java");
-            add("Scala");
-            add("Php");
-        }};
+    public void reciclerGridViewClick(View view) {
+    }
+
+    public void reciclerListViewClick(View view) {
+        Intent reciclerIntent = new Intent(ReciclerActivity.this, ReciclerListActivity.class);
+
+        startActivity(reciclerIntent);
     }
 }
