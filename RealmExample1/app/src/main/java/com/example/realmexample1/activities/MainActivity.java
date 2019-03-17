079c7c09ef80ab81392ba0a1ae87749dfdc8cb9c
rabejon <rabejon@kernet.es>
2019-03-17 20:15:18 +0100

    Fab material desing

--- RealmExample1/app/src/main/java/com/example/realmexample1/activities/MainActivity.java
+++ RealmExample1/app/src/main/java/com/example/realmexample1/activities/MainActivity.java
@@ -1,25 +1,35 @@
 package com.example.realmexample1.activities;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Person;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.realmexample1.R;
-import com.example.realmexample1.models.Persona;
+import com.example.realmexample1.models.Board;
+import com.example.realmexample1.models.Note;
 
 import io.realm.RealmChangeListener;
 import io.realm.RealmResults;
 
-public class MainActivity extends AppCompatActivity implements RealmChangeListener<RealmResults<Persona>> {
+public class MainActivity extends AppCompatActivity implements RealmChangeListener<RealmResults<Note>> {
+
+    private FloatingActionButton button ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.fabAddBoard);
+
+        //button.setOnClickListener();
+
     }
 
+
     @Override
-    public void onChange(RealmResults<Persona> personas) {
+    public void onChange(RealmResults<Note> notes) {
 
     }
 }
