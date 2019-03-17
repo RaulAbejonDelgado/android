5f04d7c76e7ef63583517279fbb2ad7d0b2419c8
rabejon <rabejon@kernet.es>
2019-03-17 20:35:43 +0100

    PopUp Dialog

--- RealmExample1/app/src/main/java/com/example/realmexample1/activities/MainActivity.java
+++ RealmExample1/app/src/main/java/com/example/realmexample1/activities/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.realmexample1.activities;
 
+import android.content.DialogInterface;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Person;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.realmexample1.R;
 import com.example.realmexample1.models.Board;
@@ -23,8 +30,46 @@ public class MainActivity extends AppCompatActivity implements RealmChangeListen
 
         button = findViewById(R.id.fabAddBoard);
 
-        //button.setOnClickListener();
 
+        showAlertForCreatingBoard("Title","Message");
+    }
+
+    private void showAlertForCreatingBoard(String title, String message){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        if(title != null){
+            builder.setTitle(title);
+        }
+        if(message != null){
+            builder.setMessage(message);
+        }
+
+        View viewLayoutInflater = LayoutInflater.from(this).inflate(R.layout.dialog_create_board,null);
+
+        builder.setView(viewLayoutInflater);
+
+        final EditText input = (EditText) findViewById(R.id.editTextBoard);
+
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String boardName = input.getText().toString().trim();
+                if(boardName.length() > 0){
+                    createNewBoard(boardName);
+                }else{
+                    Toast.makeText(getApplicationContext(),"Name required", Toast.LENGTH_SHORT);
+                }
+
+            }
+        });
+
+        builder.create().show();
+
+
+    }
+
+    private void createNewBoard(String boardName) {
     }
 
 
